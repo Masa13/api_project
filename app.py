a8137b4c0ec95db3cc6ef4029048a7020e7b0457
Shawn Li <shawnli050298@gmail.com>
2015-11-06 13:12:14 -0500

    Added tag route

--- app.py
+++ app.py
@@ -10,6 +10,23 @@ def home():
 
 #@app.route("/anime
 
+@app.route("/t")
+@app.route("/t/<tag>")
+def t(tag="search"):
+    url="http://myanimelist.net/api/anime/search.xml?tag=%s"
+    request = urllib2.urlopen(url)
+    result = request.read()
+    r = json.loads(result)
+    moviePosters = []
+    for poster in r['response']:
+        try:
+            moviePosters.append(poster['image_url'])
+        except:
+            pass
+
+    return render_template("moviePosters.html", urls=moviePosters)
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run(host="0.0.0.0", port=9000)
