f01b9302e7d5b24fbf96f9998462bddd494b0b40
Masa13 <masahero55@gmail.com>
2015-11-10 11:46:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,21 @@ def home():
 @app.route("/t")
 @app.route("/t/<tag>")
 def t(tag="america"):#searchEncoded):
-    searchEncoded = urllib.quote(tag)
-    rawData = urllib.urlopen('https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q='+searchEncoded).read()
+    #searchEncoded = urllib.quote(tag)
+    #rawData = urllib.urlopen('https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q='+searchEncoded).read()
+    key="6qjbDDaQ4vUogvpFIZ2UoaHuo6ykn1vMpjRYOdYOPCQI6dBw4K"
+    uri="https://api.tumblr.com/v2/tagged?tag=%s&api_key=%s"
+    url = uri%(tag,key)
+
+    request = urllib2.urlopen(url)
+    rawData = request.read()
     jsonData = json.loads(rawData)
+    
     searchResults = jsonData['responseData']['results']
     images = []
     for results in searchResults:
         try:
-            images.append(results['url'])
+            images.append(['photos'][0]['original_size']['url'])#results['url'])
         except:
             pass
     return render_template("images.html",urls=images);
