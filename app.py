41974969b3572eb09ada5908472e496256e12a35
Masa13 <masahero55@gmail.com>
2015-11-16 02:42:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-search = 'banana'
-searchEncoded = urllib.quote(search)
+#search = 'banana'
+#searchEncoded = urllib.quote(search)
 
 @app.route("/")
 @app.route("/home")
@@ -12,11 +12,28 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/search")
-@app.route("/search/<tag>")
-def t(tag=searchEncoded):
+#@app.route("/search")
+#@app.route("/search/<tag>")
+#def t(tag=searchEncoded):
+#    rawData = urllib.urlopen("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+tag).read()
+#    jsonDATA = json.loads(rawData)
+#    searchResults = jsonDATA["responseData"]["results"]
+#    images = []
+#    for result in searchResults:
+#        try:
+#            images.append(result['url'])
+#        except:
+#            pass
+#    return render_template("images.html",urls=images)
+
+@app.route("/search", methods=["GET","POST"])
+@app.route("/search/<tag>", methods=["GET","POST"])
+def search(tag1=searchEncoded):
+    keyword1=request.form['keyword1']
+    keyword2=request.form['keyword2']
+    search=keyword1+" "+keyword2
+    searchEncoded = urllib.quote(search)
     rawData = urllib.urlopen("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+tag).read()
-    jsonDATA = json.loads(rawData)
     searchResults = jsonDATA["responseData"]["results"]
     images = []
     for result in searchResults:
@@ -25,11 +42,8 @@ def t(tag=searchEncoded):
         except:
             pass
     return render_template("images.html",urls=images)
-
-@app.route("/search", methods=["GET","POST"])
-def search():
     if request.method == 'GET':
-        return render_template(")
+        return render_template("images.html", urls=images)
 
 if __name__ == "__main__":
     app.debug = True
