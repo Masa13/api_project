59ffe48c6697a3423524c1afa06dd912b45b0c1f
Masa13 <masahero55@gmail.com>
2015-11-16 03:08:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,30 @@ searchEncoded = urllib.quote(search)
 @app.route("/home")
 def home():
     return render_template("home.html")
-@app.route("/browse")
-def browse():
-    return render_template("search.html")
+
+@app.route("/browse", methods=["GET", "POST"])
+@app.route("/browse/<tag>", methods=["GET", "POST"])
+def browse(tag=searchEncoded):
+    if request.method == "GET":
+        return render_template("search.html")
+    else:
+        keyword1=request.form['keyword1']
+        keyword2=request.form['keyword2']
+        search=keyword1+" "+keyword2
+        searchEncoded = urllib.quote(search)
+        rawData = urllib.urlopen("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+tag).read()
+        jsonDATA = json.loads(rawData)
+        searchResults = jsonDATA["responseData"]["results"]
+        images = []
+        for result in searchResults:
+        try:
+            images.append(result['url'])
+        except:
+            pass
+        if button == "submit":
+            return render_template("images.html", urls=images)
+            
+        
 #@app.route("/search")
 #@app.route("/search/<tag>")
 #def t(tag=searchEncoded):
@@ -27,26 +48,6 @@ def browse():
 #            pass
 #    return render_template("images.html",urls=images)
 
-@app.route("/search", methods=["GET","POST"])
-@app.route("/search/<tag>", methods=["GET","POST"])
-def search(tag1=searchEncoded):
-    keyword1=request.form['keyword1']
-    keyword2=request.form['keyword2']
-    search=keyword1+" "+keyword2
-    searchEncoded = urllib.quote(search)
-    rawData = urllib.urlopen("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+tag).read()
-    jsonDATA = json.loads(rawData)
-    searchResults = jsonDATA["responseData"]["results"]
-    images = []
-    for result in searchResults:
-        try:
-            images.append(result['url'])
-        except:
-            pass
-    if request.method == 'GET':
-        return render_template("images.html", urls=images)
-    else:
-        return render_template("home.html")
 
 if (__name__ == "__main__"):
    app.debug = True
